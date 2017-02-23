dd1ecf323626a0e9411e5d6ee808c0fd99763874
Alexander Porrello <alexporrello@gmail.com>
2017-02-23 14:08:00 -0600

    Swapping now works across multiple tabs.

--- src/tabs/FancyTab2.java
+++ src/tabs/FancyTab2.java
@@ -2,6 +2,8 @@ package tabs;
 
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.util.function.Consumer;
 
 import javax.swing.JComponent;
 
@@ -14,23 +16,50 @@ public class FancyTab2 {
 	public int width;
 	public int height;
 
+	public Rectangle xClickPosn;
+	
 	public JComponent content;
 
 	public int index;
 
+	public Consumer<MouseEvent> listener;
+	
 	public FancyTab2(String title, int width, int height, 
 			int posn, JComponent content) {
-		this.text     = title;
-		this.width    = width;
-		this.height   = height;
-		this.content  = content;
-		this.index     = posn;
-		this.y        = 0;
-		this.x        = determineXPosition();
+		this.text       = title;
+		this.width      = width;
+		this.height     = height;
+		this.content    = content;
+		this.index      = posn;
+		this.y          = 0;
+		this.x          = determineXPosition();
+		this.xClickPosn = determineXClickPosn();
 	}
-
+	
+	public FancyTab2(String title, int width, int height, 
+			int posn, JComponent content, Consumer<MouseEvent> listener) {
+		this.text       = title;
+		this.width      = width;
+		this.height     = height;
+		this.content    = content;
+		this.index      = posn;
+		this.y          = 0;
+		this.x          = determineXPosition();
+		this.xClickPosn = determineXClickPosn();
+		this.listener   = listener;
+	}
+	
 	public int determineXPosition() {
 		return (index * width) + index;
+	}
+	
+	public Rectangle determineXClickPosn() {
+		int leftX  = x + ((width/7)*6);
+		int leftY  = 7;
+		int width  = this.width/15;
+		int height = this.height-(this.height/3);
+		
+		return new Rectangle(leftX, leftY, width, height);
 	}
 
 	/**
--- src/tabs/FancyTabPanelV2.java
+++ src/tabs/FancyTabPanelV2.java
@@ -1,11 +1,13 @@
 package tabs;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,59 +20,57 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.Timer;
 
+import colors.FancyColor;
 import displays.FancyFrame;
 import displays.FancyPanel;
 
 public class FancyTabPanelV2 extends FancyPanel {
 	private static final long serialVersionUID = 3467583173655155282L;
 
-	FancyTabOrganizer tabs = new FancyTabOrganizer();
-	
-	JComponent inView = new FancyPanel();
+	private FancyTabOrganizer tabs = new FancyTabOrganizer();
+
+	private JComponent inView = new FancyPanel();
+
+	private Color xColor = FancyColor.DARK_GRAY;
 	
 	public FancyTabPanelV2() {
 		tabs.addTab("Testing", test(Color.RED));
 		tabs.addTab("Testing 2", test(Color.BLUE));
 		tabs.addTab("Testing 3", test(Color.ORANGE));
 		
-		setLayout(new BorderLayout());
+		tabs.tabs.selected = tabs.tabs.get(0);
 		
+		setLayout(new BorderLayout());
+
 		add(tabs, BorderLayout.NORTH);
 		add(inView, BorderLayout.CENTER);
-		
+
 		addMouseListener();
 		addMouseMotionListener();
 	}
-	
+
 	public FancyPanel test(Color color) {
 		FancyPanel fp = new FancyPanel();
 		fp.setBackground(color);
 		return fp;
 	}
-	
+
 	public void switchJComponentInView(FancyTab2 newTab) {
 		remove(inView);
 		inView = newTab.content;
 		add(inView, BorderLayout.CENTER);
-		
+
 		revalidate();
 		repaint();
 	}
-	
+
 	public void addMouseListener() {
 		tabs.addMouseListener(new MouseAdapter() {
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				tabs.mouseClickPosn = e.getX();		
-
-				if(tabs.swappedRight) {
-					tabs.swapRightReleaseTimer.start();
-				} else if(tabs.swappedLeft) {
-					tabs.swapLeftReleaseTimer.start();
-				} else {
-					tabs.returnTimer.start();
-				}
+				tabs.mouseClickPosn = e.getX();
+				tabs.returnTimer.start();
 			}
 
 			@Override
@@ -81,14 +81,36 @@ public class FancyTabPanelV2 extends FancyPanel {
 				tabs.tabs.setSelectedIndex(tabs.pressedTab);
 
 				switchJComponentInView(tabs.pressedTab);
-
+				
 				repaint();
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {				
+				if (tabs.tabs.selected.determineXClickPosn().contains(e.getPoint())) {
+					//TODO Make the tab closing functionality.
+				}
 			}
 		});
 	}
+	
+	
 
 	public void addMouseMotionListener() {
 		tabs.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent arg0) {
+				if(tabs.tabs.selected != null) {
+					if(tabs.tabs.selected.determineXClickPosn().contains(arg0.getPoint())) {
+						xColor = FancyColor.DARK_RED;
+					} else {
+						xColor = FancyColor.DARK_GRAY;
+					}
+					
+					repaint();
+				}
+			}
+			
 			@Override
 			public void mouseDragged(MouseEvent arg0) {
 				tabs.pressedTab.x = arg0.getX() - tabs.mouseClickPosn;
@@ -118,7 +140,7 @@ public class FancyTabPanelV2 extends FancyPanel {
 			}
 		});
 	}
-	
+
 	/**
 	 * This is where all of the tab magic happens.
 	 * @author Alexander Porrello
@@ -135,8 +157,6 @@ public class FancyTabPanelV2 extends FancyPanel {
 
 		Timer swapRightTimer;
 		Timer swapLeftTimer;
-		Timer swapRightReleaseTimer;
-		Timer swapLeftReleaseTimer;
 		Timer returnTimer;
 
 		FancyTab2 pressedTab;
@@ -145,10 +165,14 @@ public class FancyTabPanelV2 extends FancyPanel {
 		Boolean swappedRight = false;
 		Boolean swappedLeft  = false;
 
+		Font tabFont;
+		
 		public FancyTabOrganizer() {
 			setPreferredSize(new Dimension(500, tabHeight + 5));
 
-			makeSwapReleaseTimers();
+			Font a = new JButton().getFont();
+			tabFont = new Font(a.getName(), a.getStyle(), 12);
+			
 			makeSwapTimers();
 			makeReturnTimer();
 		}
@@ -186,11 +210,12 @@ public class FancyTabPanelV2 extends FancyPanel {
 				@Override
 				public void actionPerformed(ActionEvent ae) {
 					swappedRight = true;
-
+					
 					if(swappedTab.x > pressedTab.determineXPosition()) {
 						swappedTab.x = swappedTab.x - 1;
 						repaint();
 					} else {
+						swapTabIndices(pressedTab, swappedTab);
 						swapRightTimer.stop();
 					}
 				}
@@ -200,50 +225,13 @@ public class FancyTabPanelV2 extends FancyPanel {
 				@Override
 				public void actionPerformed(ActionEvent ae) {
 					swappedLeft = true;
-
+										
 					if(swappedTab.x < pressedTab.determineXPosition()) {
 						swappedTab.x = swappedTab.x + 1;
 						repaint();
 					} else {
+						swapTabIndices(pressedTab, swappedTab);
 						swapLeftTimer.stop();
-					}
-				}
-			});
-		}
-
-		/**
-		 * Initializes the timers that are responsible for setting a released
-		 *  tab in its proper position.
-		 */
-		public void makeSwapReleaseTimers() {
-			swapRightReleaseTimer = new Timer(1, new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent ae) {
-					swappedRight = false;
-
-					if(pressedTab.x < swappedTab.determineXPosition()) {
-						pressedTab.x = pressedTab.x + 1;
-
-						repaint();
-					} else {
-						swapTabIndices(pressedTab, swappedTab);
-						swapRightReleaseTimer.stop();
-					}
-				}
-			});
-
-			swapLeftReleaseTimer = new Timer(1, new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent ae) {
-					swappedLeft = false;
-
-					if(pressedTab.x > swappedTab.determineXPosition()) {
-						pressedTab.x = pressedTab.x - 1;
-
-						repaint();
-					} else {
-						swapTabIndices(pressedTab, swappedTab);
-						swapLeftReleaseTimer.stop();
 					}
 				}
 			});
@@ -261,40 +249,44 @@ public class FancyTabPanelV2 extends FancyPanel {
 			a.index = indexB;
 			b.index = indexA;
 		}
-
 		
-
 		@Override
 		public void paintComponent(Graphics g) {
 			Graphics2D gg = (Graphics2D) g;
-			gg.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+			gg.setFont(tabFont);
+			gg.setRenderingHint(RenderingHints.KEY_RENDERING,
+					RenderingHints.VALUE_RENDER_QUALITY);
 
 			gg.setColor(Color.LIGHT_GRAY);
 			gg.fillRect(0, 0, getWidth(), getHeight());
-
-			Font a = new JButton().getFont();
-			gg.setFont(new Font(a.getName(), a.getStyle(), 13));
 
 			for(FancyTab2 tab : tabs) {
 				gg.setColor(Color.LIGHT_GRAY);
 				gg.fillRect(tab.x, tab.y, tabWidth, tabHeight);
 				gg.setColor(Color.BLACK);
 				gg.drawString(tab.text, tab.x + 6, tab.y + tabHeight-8);
-				
-//				if(tabs.getRightTab(tab).index != tabs.selected.index
-//						&& tab.index != tabs.selected.index) {
-//					gg.fillRect(1, 5, 1, 5);
-//				}
-						
 			}
 
 			gg.setColor(Color.decode("#F0F0F0"));
 			gg.fillRect(0, tabHeight, getWidth(), 5);
 
 			if(tabs.selected != null) {
-				gg.fillRect(tabs.selected.x, tabs.selected.y, tabWidth, tabHeight);
+				//Draw the selected tab in the front.
+				gg.fillRect(tabs.selected.x, tabs.selected.y, tabWidth,
+						tabHeight);
 				gg.setColor(Color.BLACK);
-				gg.drawString(tabs.selected.text, tabs.selected.x + 6, tabs.selected.y + tabHeight-8);
+				gg.drawString(tabs.selected.text, tabs.selected.x + 6, 
+						tabs.selected.y + tabHeight-8);
+				
+				//Draw out the "X"
+				gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				Rectangle r = tabs.selected.determineXClickPosn();
+				gg.setColor(xColor);
+				gg.setStroke(new BasicStroke(1, BasicStroke.CAP_ROUND, 
+						BasicStroke.JOIN_ROUND));
+				gg.drawLine(r.x, r.y, r.x+r.width, r.height);
+				gg.drawLine(r.x, r.height, r.x+r.width, r.y);
 			}
 		}
 	}
